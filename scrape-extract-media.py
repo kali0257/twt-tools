bd1f25b3ac6fac0a860ebf3073902a58c048fe2e
Kali <83681231+kali0257@users.noreply.github.com>
2021-11-03 23:16:38 -0400

    adding scrape and extract script
    read the comment

---
+++ scrape-extract-media.py
@@ -0,0 +1,28 @@
+# this obviously requires twint and urlextract to be installed
+
+import os
+import sys
+from urlextract import URLExtract
+
+extractor = URLExtract()
+
+search = input("search:  ")
+
+print("scraping...")
+os.system(f"twint --media -s {search} > {search}.txt")
+print("\n\ndone scraping, now extracting links")
+
+file = open(f"{search}.txt", 'r')
+lines = file.readlines()
+outputFile = open(f"{search}-links.txt", 'w')
+# Strips the newline character
+for line in lines:
+    for link in extractor.gen_urls(line):
+        #print(url)
+        outputFile.write(f"{link}\n")
+
+
+print("extraction is finished")
+file.close()    
+outputFile.close()
+
